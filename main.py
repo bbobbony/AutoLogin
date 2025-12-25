7736e7597cded1b51c4796fbd15518c9c729b55b
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-12-25 23:04:57 +0900

    재 주석처리

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import os
 import time
 
 def run_hero():
+    # 영웅문 Global의 실행파일 경로
     hero_path = r"C:\KiwoomGlobal\bin\NFStarter.exe"
     
     if os.path.exists(hero_path):
-        print("✅ 영웅문을 실행합니다...")
+        print("영웅문을 실행")
         os.startfile(hero_path)
         
-        print("로그인 창이 유지되는지 확인하세요. (이 창을 끄지 마세요)")
+        print("로그인 창 유지 확인")
         # 파이썬이 종료되지 않도록 무한 대기합니다.
         try:
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
-            print("프로그램을 종료합니다.")
+            print("프로그램 종료")
     else:
-        print("❌ 경로를 찾을 수 없습니다.")
+        print("경로를 찾을 수 없음")
 
+#메인함수
 if __name__ == "__main__":
+    # 함수호출
     run_hero()
