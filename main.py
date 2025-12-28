4b2e37c3198d1d85b9f49c115d06a65806f9fee3
bbobbony <142016790+bbobbony@users.noreply.github.com>
2025-12-29 03:06:48 +0900

    연결창의 내부구조 터미널에 출력

--- main.py
+++ main.py
@@ -35,9 +35,10 @@ def run_and_login_hero():
         dlg = app.window(title_re=window_title)
         dlg.set_focus()
 
+        # 연결된 창에서 내부 구조 불러오기
         print("-" * 60)
-        print("[시스템 분석] 로그인 창 컨트롤 스캔...")
-        # dlg.print_control_identifiers() # 필요할 때만 주석 해제
+        print("[시스템 분석] 영웅문 로그인 창의 내부 컨트롤 구조를 스캔...")
+        dlg.print_control_identifiers()  # 터미널에 설계도를 출력하는 핵심 명령어
         print("-" * 60)
 
         print("비밀번호 칸에 입력 중...")
